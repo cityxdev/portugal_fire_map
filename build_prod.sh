fc23c8f8fc41e8cb805f34b2912a180de3a96cd0
snunes <sergio.a.s.nunes@gmail.com>
2022-09-17 18:52:55 +0200

    avoid redirect from / to /dashboard

--- build_prod.sh
+++ build_prod.sh
@@ -3,3 +3,6 @@ npm run copy
 parcel build --target dashboard --no-cache --public-url ./
 parcel build --target webmap --no-cache --public-url ./
 parcel build --target about --no-cache --public-url ./
+cp dist/dashboard/*.js dist/
+cp dist/dashboard/*.css dist/
+cp dist/dashboard/*.jpg dist/
